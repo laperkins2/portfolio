ec565e5e8b9515683e14423a313c336ad169bf9c
laperkins2 <67718788+laperkins2@users.noreply.github.com>
2024-03-19 19:39:44 -0500

    added a table to purchase page

---
+++ booksale.js
@@ -0,0 +1,59 @@
+let addUp = 0;
+let total = 0;
+let cart = {};
+
+if (localStorage.getItem('addUp')) {
+  addUp = parseInt(localStorage.getItem('addUp'));
+}
+
+if (localStorage.getItem('total')) {
+  total = parseInt(localStorage.getItem('total'));
+}
+
+if (localStorage.getItem('cart')) {
+  cart = JSON.parse(localStorage.getItem('cart'));
+}
+
+updateCart();
+
+let btns = document.querySelectorAll('.col button');
+
+for (let i = 0; i < btns.length; i++) {
+  let btn = btns[i];
+  btn.addEventListener('click', add);
+}
+
+function add(event) {
+  let price = Number(event.target.dataset.price);
+  let title = event.target.dataset.title;
+  let id = event.target.dataset.id;
+
+  if (id in cart) {
+    cart[id].qty++;
+  } else {
+    let cartItem = {
+      title: title,
+      price: price,
+      qty: 1,
+    };
+    cart[id] = cartItem;
+  }
+
+  addUp++;
+  total += price;
+
+  console.log(cart);
+
+  localStorage.setItem('cart', JSON.stringify(cart));
+  updateCart();
+}
+
+function updateCart() {
+  document.getElementById('total').textContent = total;
+  document.getElementById('addUp').textContent = addUp;
+  localStorage.setItem('total', total);
+  localStorage.setItem('addUp', addUp);
+}
+function deleteItems() {
+  localStorage.clear();
+}
